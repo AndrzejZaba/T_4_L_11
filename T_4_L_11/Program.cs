f0d222cc32b1324c610920f4227470c330f6250f
AndrzejZaba <andrzejzaba@onet.pl>
2023-05-15 18:06:49 +0200

    Add project files.

---
+++ T_4_L_11/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace T_4_L_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Podaj liczbę, którą chcesz sprawdzić: ");
+                if (int.TryParse(Console.ReadLine(), out int number))
+                {
+                    if (Math.Abs(number) % 2 > 0) Console.WriteLine($"Liczba {number} jest nieparzysta\n");
+                    else Console.WriteLine($"Liczba {number} jest parzysta\n");
+                }
+                else
+                {
+                    Console.WriteLine("Podana wartość jest niepoprawna.\n");
+                }
+            }
+            
+        }
+    }
+}
